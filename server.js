1d3fed8dc4f3579e9aedd3b85512e3f0a67be168
eMostyn <56824767+eMostyn@users.noreply.github.com>
2020-03-04 01:10:28 +0000

    Addition of modal

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 app.use(express.static('static'));
 
 var learnInfo = "Test"
-app.get('learnMore', function (req, res) {
+app.get('/learnMore', function (req, res) {
   res.send(learnInfo)
 })
 app.listen(8090)
--- static/script.js
+++ static/script.js
@@ -1,25 +1,20 @@
-//
-// document.getElementById("moreButton").addEventListener("click", learnMore());
-// function learnMore(){
-//   console.log('here');
-//   fetch('http://127.0.0.1:8090/learnMore')
-//    .then(function(response) {
-//      if (response.ok) {
-//        return response.text();
-//      }
-//      else {
-//        return "shit the bed";
-//      }
-//    })
-//    .then(body =>
-//       window.alert(body))
-//
-//     .catch(() => {
-//         console.error("Error");
-//     })
-// }
-
-console.log('started');
-document.getElementById("myBtn").addEventListener("click", function(){
+document.getElementById("learnModal");
+document.getElementById("moreButton").addEventListener("click", learnMore());
+function learnMore(){
   console.log('here');
-});
+  fetch('http://127.0.0.1:8090/learnMore')
+   .then(function(response) {
+     if (response.ok) {
+       return response.text();
+     }
+     else {
+       return "shit the bed";
+     }
+   })
+   .then(body =>
+      learnModal.innerHTML = "SLAP A DESCRIPTION HERE")
+
+    .catch(() => {
+        console.error("Error");
+    })
+}
