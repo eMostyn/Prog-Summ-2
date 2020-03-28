52834bd4f63711d814f6807728daba21f7ba1623
eMostyn <56824767+eMostyn@users.noreply.github.com>
2020-03-28 00:40:07 +0000

    Comments of admin, squad, and stats scripts

--- static/adminScript.js
+++ static/adminScript.js
@@ -1,15 +1,19 @@
 function setAdmin(){
+  //Get the admin button element
   var adminButton = document.getElementById("adminButton");
+  //GET request to obtain the current value of admin
   fetch("http://127.0.0.1:8090/getAdmin")
   .then(function(response) {
     if (response.ok) {
       return response.text();
     }
     else {
+      //Return error if one occurs
       return "Failure to retrieve admin state";
     }
   })
   .then(body =>{
+      //Check/Uncheck the button depending on the value of admin
       adminButton.checked = JSON.parse(body);
     })
 
@@ -18,25 +22,33 @@ function setAdmin(){
 
 
 function toggleAdmin(){
+  //GET request to get the value of admin
   fetch("http://127.0.0.1:8090/getAdmin")
   .then(function(response) {
     if (response.ok) {
       return response.text();
     }
     else {
+      //Report error if one occurs
       return "Failure to retrieve admin state";
     }
   })
   .then(body =>{
+    //Get the value of admin from the response body
       var admin = JSON.parse(body);
+      //If admin is currently true
       if (admin == true) {
+        //Set it to false and alert the user
         admin = false
         window.alert("Admin mode off.")
       } else {
+        //Set it to true and alert the user
         admin = true
         window.alert("Admin mode on.")
       }
+      //Create a JSON holding the new value of admin
       adminObj = JSON.stringify({Admin: admin})
+      //Create options for POST request, setting body to new admin value
       const options = {
         method: 'POST',
         headers: {
@@ -44,13 +56,15 @@ function toggleAdmin(){
         },
         body: adminObj
       }
+      //Send POST request to update admin value
       fetch("http://127.0.0.1:8090/adminToggle",options)
       .then(function(response) {
         if (response.ok) {
           return response.text();
         }
         else {
-          return "Error in adding new event";
+          //If an error occurs report it
+          return "Error in updating admin value";
         }
       })
     })
